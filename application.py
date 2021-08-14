37c75f5d605e589bf522dd015e496ebb0e7bc7ca
Lucas <lucas.ma.dev@gmail.com>
2021-08-14 16:37:55 -0600

    Added base endpoints

---
+++ application.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+@app.route('/login', methods=['POST'])
+def login():
+    return None
+
+@app.route('/register', methods=['POST'])
+def register():
+    return None
+
+@app.route('/clips')
+def getClips():
+    return None
+
+@app.route('/clips', methods=['PUT'])
+def addClips():
+    return None
+
+@app.route('/clips/<clipid>')
+def getClip():
+    return None
+
+@app.route('/clips', methods=['DELETE'])
+def deleteClip():
+    return None
+
+@app.route('/comments/<clipid>')
+def getComments():
+    return None
+
+@app.route('/comments/<clipid>', methods=['PUT'])
+def addComment():
+    return None
+
+@app.route('/user/<userid>')
+def getUser():
+    return None
+
+@app.route('/follow/<follower>/<followee>')
+def isFollowing():
+    return None
+
+@app.route('/follow/<follower>/<followee>', methods=['PUT'])
+def follow():
+    return None
+
+@app.route('/follow/<follower>/<followee>', methods=['DELETE'])
+def unfollow():
+    return None
