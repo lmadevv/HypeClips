90ae3b2d08512792af1eb131de3ac7f6c46b4937
Lucas <lucas.ma.dev@gmail.com>
2021-08-28 19:22:33 -0600

    Got rid of some redundant comments

--- test_application.py
+++ test_application.py
@@ -37,46 +37,31 @@ class UserLogin(BaseUserTestCase):
     """
 
     def testValidLogin(self):
-        """
-        Tests that login works for a valid username
-        """
-        # Set up all the data needed
+
         db.session.add(self.createUser())
         db.session.commit()
 
-        # Call the login method
         response = self.client.post("/login", json=dict(username="bob", password="pass123"))
 
-        # Check if the response is what we wanted
         assert response.status_code == 200
 
     def testInvalidLoginWrongUsername(self):
-        """
-        Tests that login works for an invalid username
-        """
-        # Set up all the data needed
+
         db.session.add(self.createUser())
         db.session.commit()
 
-        # Call the login method with invalid username
         response = self.client.post("/login", json=dict(username="boasdb", password="pass123"))
 
-        # Check if the response is what we expected
         assert response.status_code == 404
         assert response.json["status"] == "not a valid login"
 
     def testInvalidLoginWrongPassword(self):
-        """
-        Tests that login works for an invalid password
-        """
-        # Set up all the data needed
+
         db.session.add(self.createUser())
         db.session.commit()
 
-        # Call the login method with invalid password
         response = self.client.post("/login", json=dict(username="bob", password="pass123asdasd"))
 
-        # Check if the response is what we wanted
         assert response.status_code == 404
         assert response.json["status"] == "not a valid login"
 
@@ -86,66 +71,45 @@ class UserRegister(BaseUserTestCase):
     """
 
     def testValidRegistration(self):
-        """
-        Tests for a valid registration
-        """
-        # Call the register method
+
         response = self.client.post("/register", json=dict(username="bob", password="pass123"))
 
-        # Check if the response is what we wanted
         assert response.status_code == 200
 
-        # Get the user from the database
         user = User.query.get(1)
 
-        # Check if the info is the original info
         assert user.username == "bob"
         assert user.password == "pass123"
 
     def testInvalidRegistrationAlreadyRegistered(self):
-        """
-        Tests for invalid registration, the username is already registered
-        """
-        # Set up all the data needed
+
         db.session.add(self.createUser())
         db.session.commit()
 
-        # Call the register methods with same username
         response = self.client.post("/register", json=dict(username="bob", password="123123"))
 
-        # Check if the response is what we wanted
         assert response.status_code == 400
         assert response.json["status"] == "unsuccessful registration: user with username already exists"
 
     def testInvalidRegistrationLongUsername(self):
-        """
-        Tests for invalid registration, the requested username is too long
-        """
-        # Set up all the data needed
+
         db.session.add(self.createUser())
         db.session.commit()
 
-        # Call the register methods with a username that is too long
         response = self.client.post("/register", json=dict(
             username="basdkjfasdjflkjasdflkjaskfdjaslkdfjlskadjflkasjdflkjadflkasjklfsfob", password="123123"))
 
-        # Check if the response is what we wanted
         assert response.status_code == 400
         assert response.json["status"] == "unsuccessful registration: username too long"
 
     def testInvalidRegistrationLongPassword(self):
-        """
-        Tests for invalid registration, the password is too long
-        """
-        # Set up all the data needed
+
         db.session.add(self.createUser())
         db.session.commit()
 
-        # Call the register methods with a password that is too long
         response = self.client.post("/register", json=dict(username="bob34",
                                                                 password="123121231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231231233"))
 
-        # Check if the response is what we wanted
         assert response.status_code == 400
         assert response.json["status"] == "unsuccessful registration: password too long"
 
