b2c0dc56b419a82bbb2829e37bfe7ecb06fb6a0d
Jmmxp <Jmmxp@users.noreply.github.com>
2021-09-06 23:34:47 -0600

    Fix spacing for front-end files (#7)

--- front-end/src/App.svelte
+++ front-end/src/App.svelte
@@ -1,10 +1,10 @@
 <script>
-	import { UNDEFINED_ID } from './constants.js';
+  import { UNDEFINED_ID } from "./constants.js"
   import Auth from "./Auth.svelte"
   import Main from "./Main.svelte"
   import Router from "svelte-spa-router"
   import routes from "./routes"
-	import { id } from "./store"
+  import { id } from "./store"
 </script>
 
 <Router {routes} />
