13d271f5161249de4b1ede2f6bc4d9910124c4dc
Lucas <lucas.ma.dev@gmail.com>
2021-12-21 12:38:28 -0700

    Added some methods to the user class to make follows easier

--- back-end/application.py
+++ back-end/application.py
@@ -30,6 +30,18 @@ class User(db.Model):
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
 class Clip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     clipUuid = db.Column(db.String(100), nullable=False)
